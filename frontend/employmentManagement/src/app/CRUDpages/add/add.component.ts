0ad4e2b7c0f746b45128bb09f7682ba2d66ab882
Fpanda <ferielmaamer@yahoo.fr>
2023-04-09 23:38:12 -0400

    add component

--- frontend/employmentManagement/src/app/CRUDpages/add/add.component.ts
+++ frontend/employmentManagement/src/app/CRUDpages/add/add.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CRUDservicesService } from '../crudservices.service';
 import { Int } from 'src/app/int_interface';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -15,13 +16,15 @@ export class AddComponent {
   gender='';
   salary: Int = { value: 0 };
 
-  constructor(private crudService: CRUDservicesService) { }
+  constructor(private crudService: CRUDservicesService, private router:Router) { }
 
   onAdd() {
     this.crudService.addEmployee(this.first_name, this.last_name,
       this.email, this.gender, this.salary).subscribe(result => {
       console.log(result);
-      // handle the result
+      if(result){
+        this.router.navigate(['/']);
+      }
     });
   }
 }
